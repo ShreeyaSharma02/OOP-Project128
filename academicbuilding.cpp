f5bad7839f11414931ec483c2960543dfd65f14a
Your Name <you@example.com>
2024-10-15 19:05:18 +1030

    parent file-acadmicbuildingg added

--- academicbuilding.cpp
+++ academicbuilding.cpp
@@ -1,4 +1,4 @@
 #include "AcademicBuilding.h"
-
+//parent class for building named cs and math
 AcademicBuilding::AcademicBuilding(const std::string& name, const sf::Vector2f& size, const sf::Vector2f& position, sf::Color color)
     : Building(name, size, position, color) {}
